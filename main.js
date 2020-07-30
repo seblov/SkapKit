c02ecdcf80fdbbc5c3cc31264d4ef490131f6ff7
Sebastian <lookeper2@gmail.com>
2020-07-30 18:14:54 +0200

    Completed list of regnr

--- main.js
+++ main.js
@@ -3,47 +3,499 @@ var array = {
   "fly": [
     {
       "reg": "rcn",
-      "skap": "stort"
+      "skap": "Stort Skap"
     },
     {
       "reg": "rcx",
-      "skap": "stort"
+      "skap": "Stort Skap"
     },
     {
       "reg": "rcy",
-      "skap": "stort"
+      "skap": "Stort Skap"
     },
     {
       "reg": "rcz",
-      "skap": "stort"
+      "skap": "Stort Skap"
     },
     {
       "reg": "reu",
-      "skap": "lite"
+      "skap": "Lite Skap"
     },
     {
       "reg": "rey",
-      "skap": "lite"
+      "skap": "Lite Skap"
     },
     {
       "reg": "rjx",
-      "skap": "lite"
+      "skap": "Lite Skap"
     },
     {
       "reg": "rnn",
-      "skap": "stort"
+      "skap": "Stort Skap"
     },
     {
       "reg": "rno",
-      "skap": "stort"
+      "skap": "Stort Skap"
     },
     {
       "reg": "rpa",
-      "skap": "stort"
+      "skap": "Stort Skap"
     },
     {
       "reg": "rpe",
-      "skap": "stort"
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rpf",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rpg",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rpj",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rpk",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rpl",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rpm",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rpn",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rpo",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rpr",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rpw",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rra",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rrb",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rre",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rrf",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rrg",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rrh",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rrj",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rrk",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rrl",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rrm",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rrn",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rrs",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rrt",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rru",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rrw",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rrx",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "tuj",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "tuk",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "tul",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "tum",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "dox",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "doy",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "doz",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rct",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rcw",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rer",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "res",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "ret",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rex",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rez",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rga",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rgb",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rgc",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rgd",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rge",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rgf",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rgg",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rgh",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rgi",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rgk",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rgl",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rgm",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rgn",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rgo",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rjr",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rjs",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rjt",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rju",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rnu",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rnw",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "roa",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rob",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "roc",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rod",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "roe",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "rro",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rrp",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rry",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "rrz",
+      "skap": "Stort Skap"
+    },
+    {
+      "reg": "tua",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "tuf",
+      "skap": "Lite Skap"
+    },
+    {
+      "reg": "ace",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "agp",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "aja",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "alc",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "ath",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "ayt",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "bcn",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "bio",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "boj",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "cfu",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "cqh",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "cta",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "dbv",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "efl",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "fao",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "fco",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "fnc",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "fue",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "her",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "ibz",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "jtr",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "kgs",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "lca",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "lis",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "lpa",
+      "skap": "Grønt kit"
+    },
+    {
+      "reg": "mad",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "mah",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "mjv",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "myp",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "nce",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "olb",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "pmi",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "prn",
+      "skap": "Grønt kit"
+    },
+    {
+      "reg": "psa",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "pvk",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "rak",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "rho",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "spu",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "tfs",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "tia",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "tiv",
+      "skap": "Grønt Kit"
+    },
+    {
+      "reg": "var",
+      "skap": "Grønt kit"
     }
   ]
 }
@@ -70,7 +522,7 @@ function getFlightNumber(){
             document.getElementById("output").innerHTML = array.fly[i].reg.toUpperCase().toString() + " - " + array.fly[i].skap.toString()
             break
         } else {
-            document.getElementById("output").innerHTML ="Flynummeret finnes ikke"
+            document.getElementById("output").innerHTML ="Finnes ikke/ Skal ikke ha Grønt kit"
         }
 
     }
